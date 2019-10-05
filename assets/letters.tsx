538d8801e75b11d98511dc277067400330fea320
Erty Seidohl <ertysdl+github@gmail.com>
2019-10-05 16:11:53 -0400

    Add spritemap sheet

---
+++ assets/letters.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="letters" tilewidth="32" tileheight="32" tilecount="27" columns="27">
+ <image source="placeholder_letters.png" width="864" height="32"/>
+</tileset>
