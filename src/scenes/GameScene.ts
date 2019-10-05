f583a0dbfa759bc5bab7437f07fdd42a99346b28
Erty Seidohl <ertysdl+github@gmail.com>
2019-10-05 18:16:22 -0400

    finished gate item

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -1,3 +1,5 @@
+const TEXT_DISPLAY_WINDOW_HEIGHT: number = 200;
+
 export default class GameScene extends Phaser.Scene {
     private square: Phaser.GameObjects.Rectangle;
     private exampleText: Phaser.GameObjects.Text;
@@ -5,6 +7,8 @@ export default class GameScene extends Phaser.Scene {
     private inventoryKey: Phaser.Input.Keyboard.Key;
     private cursorKeys: Phaser.Types.Input.Keyboard.CursorKeys;
     private cameraControls: Phaser.Cameras.Controls.FixedKeyControl;
+    private textDisplayWindow: Phaser.GameObjects.Text;
+    private textDisplayBuffer: string[] = [];
 
     constructor() {
         super({
@@ -19,6 +23,11 @@ export default class GameScene extends Phaser.Scene {
         this.load.tilemapTiledJSON("level_2", "../assets/level_2.json");
     }
 
+    public writeText(text: string) {
+        this.textDisplayBuffer.push(text);
+        this.textDisplayWindow.setText(this.textDisplayBuffer.join("\n"));
+    }
+
     public create() {
         this.square = this.add.rectangle(400, 400, 100, 100, 0xFFFFFF);
         this.exampleText = this.add.text(10, 10, "Hi Everybody", { font: '16px Courier', fill: '#00ff00' });
@@ -28,6 +37,12 @@ export default class GameScene extends Phaser.Scene {
 
         const map: Phaser.Tilemaps.Tilemap = this.make.tilemap({ key: 'level_1' });
         const tileset: Phaser.Tilemaps.Tileset = map.addTilesetImage('tiles', 'tiles');
+
+        // not working
+        this.textDisplayWindow = this.add.text(0, this.game.canvas.height - TEXT_DISPLAY_WINDOW_HEIGHT, "hey");
+
+        this.writeText("hello");
+        this.writeText("world");
         
         map.createStaticLayer("Map", tileset, 0, 0);
 
@@ -35,6 +50,7 @@ export default class GameScene extends Phaser.Scene {
 
         const camera = this.cameras.main;
         camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+        camera.setViewport(0, 0, this.game.canvas.width, this.game.canvas.height - TEXT_DISPLAY_WINDOW_HEIGHT);
 
         this.cameraControls = new Phaser.Cameras.Controls.FixedKeyControl({
             camera: camera,
