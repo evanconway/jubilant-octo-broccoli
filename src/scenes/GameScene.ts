abbf1dae56c209144cc363640d4edf300828041e
Erty Seidohl <ertysdl+github@gmail.com>
2019-10-05 16:36:49 -0400

    Moving layers

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -4,6 +4,7 @@ export default class GameScene extends Phaser.Scene {
     private exampleActive: boolean = true;
     private inventoryKey: Phaser.Input.Keyboard.Key;
     private cursorKeys: Phaser.Types.Input.Keyboard.CursorKeys;
+    private cameraControls: Phaser.Cameras.Controls.FixedKeyControl;
 
     constructor() {
         super({
@@ -14,6 +15,7 @@ export default class GameScene extends Phaser.Scene {
     public preload() {
         this.load.image("new_tilemap", "../assets/new_tilemap.png");
         this.load.tilemapTiledJSON("level_1", "../assets/level_1.json");
+        this.load.tilemapTiledJSON("level_2", "../assets/level_2.json");
     }
 
     public create() {
@@ -23,16 +25,30 @@ export default class GameScene extends Phaser.Scene {
         this.inventoryKey = this.input.keyboard.addKey("I");
         this.cursorKeys = this.input.keyboard.createCursorKeys();
 
-        const map: Phaser.Tilemaps.Tilemap = this.make.tilemap({ key: 'level_1' });
+        const map: Phaser.Tilemaps.Tilemap = this.make.tilemap({ key: 'level_2' });
         const tileset: Phaser.Tilemaps.Tileset = map.addTilesetImage('new_tileset', 'new_tilemap');
         map.createStaticLayer("Map", tileset, 0, 0);
+
+        const camera = this.cameras.main;
+        camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+
+        this.cameraControls = new Phaser.Cameras.Controls.FixedKeyControl({
+            camera: camera,
+            left: this.cursorKeys.left,
+            right: this.cursorKeys.right,
+            up: this.cursorKeys.up,
+            down: this.cursorKeys.down,
+            speed: 0.5
+        });
     }
 
-    public update() {
+    public update(time: number, delta: number) {
         if (Phaser.Input.Keyboard.JustDown(this.inventoryKey)) {
             this.scene.switch("inventory");
             console.log("Switch to inventory.");
         }
+
+        this.cameraControls.update(delta);
 
         //if (this.input.mouse.onMouseDown()) this.exampleActive = !this.exampleActive;
 
