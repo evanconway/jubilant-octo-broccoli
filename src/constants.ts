9f0ba3113897ebb044a1b793f53c7e2670fa3cfd
Erty Seidohl <ertysdl+github@gmail.com>
2019-10-07 12:09:42 -0400

    Properly placed letter holders

--- src/constants.ts
+++ src/constants.ts
@@ -6,9 +6,9 @@ export const MOVE_DELAY = 100; //ms
 export const LETTER_HOLDER_SIZE = 45; // pixels
 export const LETTER_HOLDER_TOP_MARGIN = -5;
 export const LETTER_HOLDER_LEFT_MARGIN = -5;
-export const LETTER_LEFT_MARGIN = 10;
+export const LETTER_LEFT_MARGIN = 64;
 
-export const INVENTORY_TOP = 64;
+export const INVENTORY_TOP = 32;
 export const ITEM_TOP = 128;
 
 export enum ItemResolutionResponse {
