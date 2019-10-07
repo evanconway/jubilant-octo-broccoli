73f2de0ccc250d23becfaaa85d663d25bc1a1e86
Erty Seidohl <ertysdl+github@gmail.com>
2019-10-06 21:43:36 -0400

    thin

--- src/levels/LevelData.ts
+++ src/levels/LevelData.ts
@@ -5,7 +5,7 @@ export class LevelData {
 
 export const LEVEL_DATA: LevelData[] = [
     {
-        validWords: new Set<string>(["nog", "hog", "g", "hot", "hit"]),
+        validWords: new Set<string>(["nog", "hog", "g", "hot", "hit", "thin"]),
         textAreas: new Map<string, string>([
             ["4,6", "This icy wall needs some heat to melt away!"],
             ["10,6", "If you could lose some weight, you might be able to slip between the bars..."]
