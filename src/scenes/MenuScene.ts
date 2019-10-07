50b83fb8c9cc47a74cb9170d06bb38fb91b6de00
Erty Seidohl <ertysdl+github@gmail.com>
2019-10-07 15:45:14 -0400

    Loading scene

--- src/scenes/MenuScene.ts
+++ src/scenes/MenuScene.ts
@@ -1,4 +1,5 @@
 import GameScene from "./GameScene";
+import LevelLoader from "../levels/LevelLoader";
 
 const LEFTX: number = 100;
 const STARTY: number = 100;
@@ -22,18 +23,11 @@ export default class MenuScene extends Phaser.Scene {
         this.load.spritesheet('letters', 'assets/letters.png', { frameWidth: 32, frameHeight: 32 });
         this.load.spritesheet('letter_holder', 'assets/letter_holder.png', { frameWidth: 45, frameHeight: 45 });
 
-        let i = 0;
-        this.add.text(LEFTX, STARTY, "N O T H I N G");
-        i++;
-        this.add.text(LEFTX, STARTY + i++ * LINE_HEIGHT, "Use the arrow keys to move.");
-        this.add.text(LEFTX, STARTY + i++ * LINE_HEIGHT, "Type to use letters from your inventory.");
-        this.add.text(LEFTX, STARTY + i++ * LINE_HEIGHT, "`'Backspace' puts a letter back.`");
-        this.loadText = this.add.text(LEFTX, STARTY + i++ * LINE_HEIGHT, "Loading 0");
-
+        this.loadText = this.add.text(LEFTX, STARTY + LINE_HEIGHT, "Loading:  0%");
 
         this.load.on('progress', (value: any) => {
             if (this.loadText) {
-                this.loadText.setText(`Loading: ${value * 100}`);
+                this.loadText.setText(`Loading: ${value * 100}%`);
             }
         });
         
@@ -46,6 +40,14 @@ export default class MenuScene extends Phaser.Scene {
     }
 
     public create() {
+        LevelLoader.asyncLoadTilemap(this, "assets/menu.json").then((tileMap) => {
+            let i = 0;
+            this.add.text(LEFTX, STARTY + i++ * LINE_HEIGHT, "Use the arrow keys to move.");
+            this.add.text(LEFTX, STARTY + i++ * LINE_HEIGHT, "Type to use letters from your inventory.");
+            this.add.text(LEFTX, STARTY + i++ * LINE_HEIGHT, "`'Backspace' puts a letter back.`");
+            this.add.text(LEFTX, STARTY + i++ * LINE_HEIGHT, "Click or press a key to start.");
+        });
+
         this.input.on('pointerup', () => {
             if (this.isLoaded) {
                 this.scene.switch('game');
