e22c3f883ab05d4581f2592326e9f9db37b5bec5
AceOfHeart5 <evanconwaymusic@gmail.com>
2019-10-06 23:31:21 -0400

    Can't input oppposite directions.

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -107,13 +107,13 @@ export default class GameScene extends Phaser.Scene {
           return false;
         }
 
-        if (this.cursorKeys.up.isDown) {
+        if (this.cursorKeys.up.isDown && !this.cursorKeys.down.isDown) {
            return this.executeMoveAction(player, playerTileX, playerTileY - 1, "up");
-        } else if (this.cursorKeys.down.isDown) {
+        } else if (this.cursorKeys.down.isDown && !this.cursorKeys.up.isDown) {
            return this.executeMoveAction(player, playerTileX, playerTileY + 1, "down");
-        } else if (this.cursorKeys.left.isDown) {
+        } else if (this.cursorKeys.left.isDown && !this.cursorKeys.right.isDown) {
            return this.executeMoveAction(player, playerTileX - 1, playerTileY, "left");
-        } else if (this.cursorKeys.right.isDown) {
+        } else if (this.cursorKeys.right.isDown && !this.cursorKeys.left.isDown) {
            return this.executeMoveAction(player, playerTileX + 1, playerTileY, "right");
         }
 
